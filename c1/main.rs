c6c6b01a95de1e8df17c805898a08d538a1b64c8
Akshith Ambekar <123666607+akshithambekar@users.noreply.github.com>
2026-06-24 22:26:30 -0400

    chapter 1

---
+++ c1/main.rs
@@ -0,0 +1,5 @@
+fn main() { // always the first thing that runs in an executable Rust program
+    println!("Hello, world!");
+    // macros let you write code to generate code to extend Rust syntax
+    // ! = macro. no ! = function
+}
