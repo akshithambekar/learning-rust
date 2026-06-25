ebb8ee2f0b92fadc9745aea27c0af399a4a2ca75
Akshith Ambekar <123666607+akshithambekar@users.noreply.github.com>
2026-06-24 22:27:43 -0400

    miscellaneous testing

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// cargo build - build executable, follow with ./target/debug/<exec_name>
+// cargo run - rebuild if needed, then run executable
+// cargo check - check for errors without building
+
+// cargo build --release builds the executable in /target/release instead
